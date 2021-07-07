4d3237625c2ccb56dfae1993fd19e60ed6edee41
Giorgio Giannotta <jorge_upa@msn.com>
2021-07-07 11:41:21 +0200

    Initial Commit

---
+++ TabBar Menu/TabBar_MenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabBar_MenuApp.swift
+//  TabBar Menu
+//
+//  Created by Jorge Giannotta on 07/07/21.
+//
+
+import SwiftUI
+
+@main
+struct TabBar_MenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
